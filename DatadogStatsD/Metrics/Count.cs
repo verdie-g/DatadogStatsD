c694f6238dec900986d911339cdacab8eb63ad10
verdie-g <gregoire.verdier@gmail.com>
2020-03-07 15:03:18 +0100

    Add metric base type

--- DatadogStatsD/Metrics/Count.cs
+++ DatadogStatsD/Metrics/Count.cs
@@ -9,43 +9,23 @@ namespace DatadogStatsD.Metrics
     /// number of events can accumulate or decrease over time — it is not monotonically increasing.
     /// </summary>
     /// <remarks>Documentation: https://docs.datadoghq.com/developers/metrics/types?tab=count</remarks>
-    public class Count
+    public class Count : Metric
     {
         private static readonly byte[] TypeBytes = DogStatsDSerializer.SerializeMetricType(MetricType.Count);
 
-        private readonly ITransport _transport;
-        private readonly byte[] _metricNameBytes;
-        private readonly double _sampleRate;
-        private readonly byte[] _sampleRateBytes;
-        private readonly byte[] _tagsBytes;
-
         internal Count(ITransport transport, string metricName, double sampleRate, IList<string> tags)
+            : base(transport, metricName, sampleRate, tags, true)
         {
-            _transport = transport;
-            _metricNameBytes = DogStatsDSerializer.SerializeMetricName(metricName);
-            _sampleRate = sampleRate;
-            _sampleRateBytes = DogStatsDSerializer.SerializeSampleRate(sampleRate);
-            _tagsBytes = DogStatsDSerializer.SerializeTags(tags);
         }
 
         public void Increment(long delta = 1)
         {
-            if (!Sampling.Sample(_sampleRate))
-                return;
-
-            var metricBytes = DogStatsDSerializer.SerializeMetric(_metricNameBytes, delta, TypeBytes,
-                _sampleRateBytes, _tagsBytes);
-            _transport.Send(metricBytes);
+            Send(delta, TypeBytes);
         }
 
         public void Decrement(long delta = 1)
         {
-            if (!Sampling.Sample(_sampleRate))
-                return;
-
-            var metricBytes = DogStatsDSerializer.SerializeMetric(_metricNameBytes, -delta, TypeBytes,
-                _sampleRateBytes, _tagsBytes);
-            _transport.Send(metricBytes);
+            Send(-delta, TypeBytes);
         }
     }
 }
---
+++ DatadogStatsD/Metrics/Metric.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using DatadogStatsD.Transport;
+
+namespace DatadogStatsD.Metrics
+{
+    public abstract class Metric
+    {
+        private readonly ITransport _transport;
+        private readonly byte[] _metricNameBytes;
+        private readonly double _sampleRate;
+        private readonly byte[] _sampleRateBytes;
+        private readonly byte[] _tagsBytes;
+
+        protected Metric(ITransport transport, string metricName, double sampleRate, IList<string> tags, bool includeSampleRate)
+        {
+            _transport = transport;
+            _metricNameBytes = DogStatsDSerializer.SerializeMetricName(metricName);
+            _sampleRate = sampleRate;
+            _sampleRateBytes = includeSampleRate ? DogStatsDSerializer.SerializeSampleRate(sampleRate) : null;
+            _tagsBytes = DogStatsDSerializer.SerializeTags(tags);
+        }
+
+        protected void Send(double value, byte[] typeBytes)
+        {
+            if (!Sampling.Sample(_sampleRate))
+                return;
+
+            var metricBytes = DogStatsDSerializer.SerializeMetric(_metricNameBytes, value, typeBytes,
+                _sampleRateBytes, _tagsBytes);
+            _transport.Send(metricBytes);
+        }
+    }
+}
