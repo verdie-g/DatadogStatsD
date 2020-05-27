91a8efe725028f85d8dbad9a6a84d370b5f02075
verdie-g <gregoire.verdier@gmail.com>
2020-05-27 23:46:41 +0200

    Implement Metric.ToString

--- DatadogStatsD/Metrics/Metric.cs
+++ DatadogStatsD/Metrics/Metric.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Text;
 using DatadogStatsD.Protocol;
 using DatadogStatsD.Telemetering;
 using DatadogStatsD.Transport;
@@ -13,9 +14,12 @@ namespace DatadogStatsD.Metrics
     {
         private readonly ITransport _transport;
         private readonly ITelemetry _telemetry;
+
+        private readonly string _metricName;
         private readonly byte[] _metricNameBytes;
         private readonly double _sampleRate;
         private readonly byte[]? _sampleRateBytes;
+        private readonly IList<string>? _tags;
         private readonly byte[] _tagsBytes;
 
         internal Metric(ITransport transport, ITelemetry telemetry, string metricName, double sampleRate,
@@ -23,9 +27,11 @@ namespace DatadogStatsD.Metrics
         {
             _transport = transport;
             _telemetry = telemetry;
+            _metricName = metricName;
             _metricNameBytes = DogStatsDSerializer.SerializeMetricName(metricName);
             _sampleRate = sampleRate;
             _sampleRateBytes = includeSampleRate ? DogStatsDSerializer.SerializeSampleRate(sampleRate) : null;
+            _tags = tags;
             _tagsBytes = DogStatsDSerializer.ValidateAndSerializeTags(tags);
         }
 
@@ -34,6 +40,28 @@ namespace DatadogStatsD.Metrics
         /// </summary>
         public virtual void Dispose()
         {
+        }
+
+        /// <summary>
+        /// Returns a string representing the <see cref="Metric"/>.
+        /// </summary>
+        public override string ToString()
+        {
+            var sb = new StringBuilder(_metricName);
+            if (_tags == null || _tags.Count == 0)
+            {
+                return sb.ToString();
+            }
+
+            sb.Append("[");
+            foreach (string tag in _tags)
+            {
+                sb.Append(tag + ",");
+            }
+
+            sb.Length -= 1; // remove last comma
+            sb.Append("]");
+            return sb.ToString();
         }
 
         protected void Send(double value, byte[] typeBytes)
