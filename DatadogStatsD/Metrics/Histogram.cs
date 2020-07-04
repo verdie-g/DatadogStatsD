a6c32c6eb1626387a35072b6c75f04191b3c464b
verdie-g <gregoire.verdier@gmail.com>
2020-07-04 10:33:12 +0200

    Remove Histogram.Record

--- DatadogStatsD/Metrics/Histogram.cs
+++ DatadogStatsD/Metrics/Histogram.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using DatadogStatsD.Telemetering;
 using DatadogStatsD.Transport;
@@ -17,12 +16,6 @@ namespace DatadogStatsD.Metrics
         }
 
         internal override MetricType MetricType => MetricType.Histogram;
-
-        [Obsolete("Use Sample instead")]
-        public void Record(double value)
-        {
-            Send(value);
-        }
 
         /// <summary>
         /// Samples a new value for current <see cref="Histogram"/> instance.
