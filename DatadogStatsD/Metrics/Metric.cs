5599ae29dceff0635e88f2c9aea0373d72543ccc
verdie-g <gregoire.verdier@gmail.com>
2020-07-04 10:32:03 +0200

    Make Metric.Send private protected

--- DatadogStatsD/Metrics/Metric.cs
+++ DatadogStatsD/Metrics/Metric.cs
@@ -66,7 +66,7 @@ namespace DatadogStatsD.Metrics
             return sb.ToString();
         }
 
-        internal void Send(double value)
+        private protected void Send(double value)
         {
             _telemetry.MetricSent();
 
